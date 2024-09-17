0b23f5f28fb1aa795edb8aae163ff31fce2f2bdd
hugo <hugo.varloud@gmail.com>
2024-09-17 19:07:13 +0200

    Legalplace - feature - break code into classes and simplify pharmacy

--- pharmacy.js
+++ pharmacy.js
@@ -6,61 +6,71 @@ export class Drug {
   }
 }
 
-export class Pharmacy {
-  constructor(drugs = []) {
-    this.drugs = drugs;
+class HerbalTeal extends Drug {
+  update () {
+    this.benefit = Math.min(50, this.benefit + 1);
+    this.expiresIn -= 1;
+    if (this.expiresIn < 0) {
+      this.benefit = Math.min(50, this.benefit + 1);
+    }
   }
-  updateBenefitValue() {
-    for (let i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
+}
+
+class Fervex extends Drug {
+  update () {
+    if (this.benefit < 50) {
+      this.benefit += 1;
+      if (this.expiresIn < 11) {
+        this.benefit += 1;
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
+      if (this.expiresIn < 6) {
+        this.benefit += 1;
       }
     }
+    this.expiresIn -= 1;
+    if (this.expiresIn < 0) {
+      this.benefit = 0;
+    }
+  }
+}
 
+class MagicPill extends Drug {
+  update () {
+    return;
+  }
+}
+
+class RegularDrug extends Drug {
+  update () {
+    if (this.benefit > 0) {
+      this.benefit -= 1;
+    }
+    this.expiresIn -= 1;
+    if (this.expiresIn < 0 && this.benefit > 0) {
+      this.benefit -= 1;
+    }
+  }
+}
+
+export class Pharmacy {
+  constructor(drugs = []) {
+    this.drugs = drugs.map(drug=>this.createDrug(drug));
+  }
+
+  createDrug(drug) {
+    switch (drug.name) {
+      case "Herbal Tea":
+        return new HerbalTeal(drug.name, drug.expiresIn, drug.benefit);
+      case "Fervex":
+        return new Fervex(drug.name, drug.expiresIn, drug.benefit);
+      case "Magic Pill":
+        return new MagicPill(drug.name, drug.expiresIn, drug.benefit)
+      default:
+        return new RegularDrug(drug.name, drug.expiresIn, drug.benefit)
+    }
+  }
+  updateBenefitValue() {
+    this.drugs.forEach(drug => drug.update());
     return this.drugs;
   }
 }
